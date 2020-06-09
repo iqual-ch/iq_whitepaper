186da8f5bca21998a5fdff9117f72a996df55f8a
Ginovski <i.ginovski@iqual.ch>
2020-06-09 11:41:58 +0200

    Added whitepaper download event

---
+++ IqWhitepaperEvents.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\iq_whitepaper;
+
+/**
+ *
+ */
+final class IqWhitepaperEvents {
+
+  /**
+   * Name of the event fired after user downloads a whitepaper.
+   */
+  const USER_DOWNLOAD_WHITEPAPER = 'iq_whitepaper.userDownloadWhitepaper';
+}
+
---
+++ src/Event/IqWhitepaperEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\iq_whitepaper\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Event that is fired when a user is handled.
+ */
+class IqWhitepaperEvent extends Event {
+
+  /**
+   * The event data for the user.
+   *
+   * @var mixed
+   */
+  protected $user = NULL;
+
+  /**
+   * Constructs the object.
+   *
+   * @param \Drupal\user\Entity\User $user
+   *   The user data.
+   */
+  public function __construct(&$user) {
+    $this->user = &$user;
+  }
+
+  /**
+   * Returns the event data for the user.
+   *
+   * @return mixed
+   */
+  public function &getUser() {
+    return $this->user;
+  }
+
+}
